e357d31b64163a6d426abf0b419fd4dcb2fcf52b
FAArik <gasgas132.fa@gmail.com>
2024-08-05 23:26:36 +0300

    createcashregistercommand start

---
+++ eMuhasebeApi/eMuhasebeApi/eMuhasebeApi.Application/Features/CashRegisterDetails/CreateCashRegisterDetail/CreateCashRegisterDetailCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace eMuhasebeApi.Application.Features.CashRegisterDetails.CreateCashRegisterDetail;
+
+public sealed record CreateCashRegisterDetailCommand():IRequest<Result<string>>;
+
+
+internal sealed class CreateCashRegisterDetailCommandHandler : IRequestHandler<CreateCashRegisterDetailCommand, Result<string>>
+{
+    public Task<Result<string>> Handle(CreateCashRegisterDetailCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
